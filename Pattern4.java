9d6fbfc06fd36abeadd3ba45612e476f88d0594b
OtteSridhar <140309309+OtteSridhar@users.noreply.github.com>
2023-07-23 14:50:29 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+class Pattern4{
+	public static void main(String[] args) {
+	    for(int i=1;i<=3;i++){
+	        for(int j=1;j<=3;j++){
+	            if(i==1||j==1||i==3){
+	                System.out.print("*");
+	            }
+	        else{
+	            System.out.print(" ");
+	        }
+	        }
+	     System.out.println();
+	        }
+	    }
+	}
+
